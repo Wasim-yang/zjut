6fb783d947542fb8423a851f833a28cdb3ea69a4
zjutdaimakushou <937039221@qq.com>
2021-01-03 21:46:19 +0800

    zjut page 1.2

--- src/main/java/com/edu/zjut/entity/Goods.java
+++ src/main/java/com/edu/zjut/entity/Goods.java
@@ -11,15 +11,6 @@ public class Goods {
     String description;
     String path;
 
-    /**
-     * 默认每页显示多少条记录
-     */
-    public static final int DEFAULT_PAGE_SIZE = 5;
-
-    /**
-     * 默认显示第几页记录
-     */
-    public static final int DEFAULT_PAGE_NUM = 1;
 
     public Goods() {
     }
