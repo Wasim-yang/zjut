a082380695f5fbcbbe9e6baafb953e0697e359a8
Wasim-yang <y985075530@qq.com>
2021-01-05 16:53:06 +0800

    整合用户列表、商品、公益、任务、优惠券

--- src/main/java/com/edu/zjut/controller/UsrController.java
+++ src/main/java/com/edu/zjut/controller/UsrController.java
@@ -21,20 +21,20 @@ public class UsrController {
 //        return usrService.insert(id, name, password, sex, age, address, cintegral);
 //    }
     /*查询,输出无密码用户信息*/
-    @RequestMapping(path = "/usr/selectNoPpage")
-    public Page<UsrNoP> selectNoP(int currentPage){ return usrService.selectNoP(currentPage); }
+    @RequestMapping(path = "/admin/usr/selectNoPpage")
+    public Page<UsrNoP> admin_selectNoP(int currentPage){ return usrService.selectNoP(currentPage); }
 
     /*按id查询,输出无密码用户信息*/
-    @RequestMapping(path = "/usr/selectNoPid")
-    public UsrNoP selectNoPid(String id){return usrService.selectNoPid(id);}
+    @RequestMapping(path = "/admin/usr/selectNoPid")
+    public UsrNoP admin_selectNoPid(String id){return usrService.selectNoPid(id);}
 
     /*删除*/
-    @RequestMapping(path = "/usr/delete")
-    public Res delete(String id){ return usrService.delete(id); }
+    @RequestMapping(path = "/admin/usr/delete")
+    public Res admin_delete(String id){ return usrService.delete(id); }
 
     /*更新*/
-    @RequestMapping(path = "/usr/updateNoP")
-    public Res updateNoP(String id, String name, int sex, int age, String address, int cintegral){
+    @RequestMapping(path = "/admin/usr/updateNoP")
+    public Res admin_updateNoP(String id, String name, int sex, int age, String address, int cintegral){
         return usrService.updateNoP(id, name, sex, age, address, cintegral);
     }
 }
